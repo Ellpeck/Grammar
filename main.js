6c34bfec4dd4c97f68272972e99c9dbf5dfe6c0c
Ellpeck <megamaximal@gmail.com>
2018-07-06 17:40:07 +0200

    make all of the grammars into a list

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const grammars = new Array();
+
 $(function() {
     let field = $('#grammar-input');
-    let display = $('#grammar-display');
     let alert = $('#grammar-alert');
     let box = $('#grammar-mode');
     let info = $('#grammar-mode-info');
@@ -10,18 +11,13 @@ $(function() {
 
         try {
             let grammar = parseGrammar(field.val(), box.is(':checked'));
+            addGrammar(grammar);
 
             alert.hide();
             alert.html('');
-
-            display.html(grammar.toString());
-            display.show();
         } catch (e) {
             alert.html(e);
             alert.show();
-
-            display.hide();
-            display.html('');
         }
     });
 
@@ -37,3 +33,46 @@ $(function() {
         }
     });
 });
+
+function addGrammar(grammar) {
+    grammars.push(grammar);
+    updateGrammarData();
+}
+
+function getSelectedGrammarId() {
+    for (let i = 0; i < grammars.length; i++) {
+        if ($('#grammar-' + i).hasClass('active')) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function updateGrammarData() {
+    let display = $('#grammars');
+    display.html('');
+    for (let i = grammars.length - 1; i >= 0; i--) {
+        let html = '<div class="list-group-item list-group-item-action" data-toggle="list" id=grammar-' + i + '>';
+        html += '<div class="row align-items-center">'
+
+        html += '<div class="col-md-11">'
+        html += grammars[i].toString();
+        html += '</div>';
+
+        html += '<div class="col-md-1">'
+        html += '<button type="button" class="btn grammar-remove" id="grammar-remove-' + i + '">X</button>'
+        html += '</div>';
+
+        html += '</div>';
+        html += '</div>';
+
+        display.append(html);
+    }
+
+    $('.grammar-remove').on('click', function() {
+        let i = this.id.replace('grammar-remove-', '');
+        grammars.splice(i, 1);
+        updateGrammarData();
+        return false;
+    });
+}
